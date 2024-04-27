c575c99b71a7716d609697ab3e3943ab99da99c5
limax <joel_bolkert@hotmail.com>
2024-04-27 19:40:16 +0200

    more backgrounds

--- src/backgrounds.js
+++ src/backgrounds.js
@@ -18,6 +18,37 @@ const backgrounds = [
         modifiers: {ST: 1, DX: -2, Repair: 2}
     },
     {
+        name: "Apprenticed to a Shopkeeper",
+        races: ["human", "elf", "dwarf", "gnome", "halfling", "halfelf", "halforc", "halfogre"],
+        description: "Spending your whole childhood inside a shop, you gain an exceptional bonus to Haggle, but you suffer a penalty to Dexterity (-1).",
+        modifiers: {DX: -1, Haggle: 3}
+    },
+    {
+        name: "Army Training",
+        races: ["human", "elf", "dwarf", "gnome", "halfling", "halfelf", "halforc"],
+        description: "You received some army training during your youth, but were discharged for having flat feet. You gain bonuses to Strength (+1), as well as slight bonuses to your Bow and Melee skills, but you have a penalty to Dexterity (-1).",
+        modifiers: {ST: 1, DX: -1, Bow: 1, Melee: 1}
+    },
+    {
+        name: "Arsonist",
+        races: ["human", "gnome", "halforc"],
+        description: "You like fire. No, you LOVE fire! Fire! Fire! FIRE! You were always picked on as a kid for being scrawny, receiving penalties to Strength (-1) and Constitution (-1), but you showed them! You studied Explosives, gaining +20 Expertise, and you burned their homes! Burned them to the ground! Ha ha! Then you ran away from home and snuck aboard the IFS Zephyr, which seemed highly flammable and an excellent target. You probably would have torched the zeppelin if it hadn't been shot down.",
+        modifiers: {ST: -1, CN: -1, Explosives: 20}
+    },
+    {
+        name: "",
+        races: ["human"],
+        description: "",
+        modifiers: {}
+    },
+
+    {
+        name: "",
+        races: ["human"],
+        description: "",
+        modifiers: {}
+    },
+    {
         name: "",
         races: ["human"],
         description: "",
