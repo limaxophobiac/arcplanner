c69bbd02fc0d3e57515ae458ec0a999534e699ea
limax <joel_bolkert@hotmail.com>
2024-04-29 04:52:28 +0200

    startet putting together

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-//import Planner from './Planner.jsx'
+import Planner from './Planner.jsx'
 import './App.css'
 import Statdisplay from './statdisplay.jsx'
 import Passivedisplay from './passivedisplay.jsx'
@@ -21,6 +21,7 @@ function App() {
     <Statdisplay name ="teststat" value={stat} assigned={stat} adder = {adder}/>
     <Statdisplay name ="teststat" value={stat2} assigned={stat2} adder = {adder2} hasPoints={false}/>
     <Passivedisplay name="testpassive" value={stat + stat2}/>
+    <Planner/>
     </>
   )
 }
--- src/Planner.jsx
+++ src/Planner.jsx
@@ -2,9 +2,45 @@ import { useState } from 'react'
 import backgrounds from './backgrounds.js'
 import races from './races.js'
 import blessings from './blessings.js'
+import Statdisplay from './statdisplay.jsx'
 import './Planner.css'
 
-const basestats = {ST: 8, CN: 8, DX: 8, BT: 8, IN: 8, WP: 8, PE: 8, CH: 8};
+
+
+
+function Planner(){
+    const [character, setCharacter] = useState(characterFactory());
+    function addStat(id, val){
+        setCharacter({...character, [id]: character[id] + val, unspent: character.unspent - val})
+    }
+    return (
+        <>
+            <div id="primary">
+                <h2>Primary Statistics</h2>
+                <Statdisplay name="ST" value={8 - !character.male + character.ST} assigned={character.ST} hasPoints={character.unspent > 0} adder = {(val) => addStat("ST", val)}/>
+                <Statdisplay name="CN" value={8 + !character.male + character.CN} assigned={character.CN} hasPoints={character.unspent > 0} adder = {(val) => addStat("CN", val)}/>
+                <Statdisplay name="DX" value={8 + character.DX} assigned={character.DX} hasPoints={character.unspent > 0} adder = {(val) => addStat("DX", val)}/>
+                <Statdisplay name="BT" value={8 + character.BT} assigned={character.BT} hasPoints={character.unspent > 0} adder = {(val) => addStat("BT", val)}/>
+                <Statdisplay name="IN" value={8 + character.IN} assigned={character.IN} hasPoints={character.unspent > 0} adder = {(val) => addStat("IN", val)}/>
+                <Statdisplay name="WP" value={8 + character.WP} assigned={character.WP} hasPoints={character.unspent > 0} adder = {(val) => addStat("WP", val)}/>
+                <Statdisplay name="PE" value={8 + character.PE} assigned={character.PE} hasPoints={character.unspent > 0} adder = {(val) => addStat("PE", val)}/>
+                <Statdisplay name="CH" value={8 + character.CH} assigned={character.CH} hasPoints={character.unspent > 0} adder = {(val) => addStat("CH", val)}/>
+            </div>
+            <div id="Skills">
+                <h2>Skills</h2>
+            </div>
+            <div id="Magic">
+                <h2>Magic</h2>
+            </div>
+            <div id="Disciplines">
+                <h2>Disciplines</h2>
+            </div>
+            <div id="derived">
+                <h2>Derived Statistics</h2>
+            </div>
+        </>
+    )
+}
 
 
 function characterFactory(){
@@ -13,7 +49,7 @@ function characterFactory(){
         race: "human",
         background: 0,
         level: 1,
-        unspent: 5,
+        unspent: 10,
         ST: 0, 
         CN: 0,
         DX: 0,
@@ -65,25 +101,6 @@ function characterFactory(){
         Smithy: 0,
         Therapeutics: 0
     };
-}
-
-function Planner(){
-    const [character, setCharacter] = useState(characterFactory());
-    return (
-        <>
-            Cosmetic <br/>
-            { "ST: " + (basestats.ST + character.ST)}
-            <br/>
-            {races.halfogre.ST}
-            <br/><br/>
-            {backgrounds[3].description}
-            <br/>
-            <br/>
-            {backgrounds[4].description}
-            <br/>
-            {backgrounds[20].description}
-        </>
-    )
 }
 
 export default Planner;
